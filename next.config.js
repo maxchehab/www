d17174a82c13cac3d24756ad125c97222bfda750
maxchehab <maxchehab@gmail.com>
2019-11-21 22:50:49 +0000

    use with blog

--- next.config.js
+++ next.config.js
@@ -1,35 +1,6 @@
-const fs = require("fs");
+const withBlog = require("with-blog");
 
-const getPathsForArticles = () =>
-  fs
-    .readdirSync("./content/articles")
-    .map(blogName => {
-      const trimmedName = blogName.substring(0, blogName.length - 3);
-      return {
-        [`/${trimmedName}`]: {
-          page: "/[slug]",
-          query: {
-            slug: trimmedName
-          }
-        }
-      };
-    })
-    .reduce((acc, curr) => ({ ...acc, ...curr }), {});
-
-module.exports = {
-  webpack: configuration => {
-    configuration.module.rules.push({
-      test: /\.md$/,
-      use: "frontmatter-markdown-loader"
-    });
-
-    return configuration;
-  },
-  async exportPathMap(defaultPathMap) {
-    return {
-      ...defaultPathMap,
-      ...getPathsForArticles()
-    };
-  },
-  poweredByHeader: false
-};
+module.exports = withBlog(
+  { contentPath: "content/", pagePath: "/" },
+  { poweredByHeader: false }
+);
