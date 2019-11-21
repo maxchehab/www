73e790f7bb126beb8790a1c1ee93d9f3bf8899dc
maxchehab <maxchehab@gmail.com>
2019-11-21 05:36:41 +0000

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,11 +2,6 @@ import React, { Component } from "react";
 
 export default class Home extends Component {
   render() {
-    const {
-      html,
-      attributes: { title, cats }
-    } = content;
-
     return <div>hello world</div>;
   }
 }
